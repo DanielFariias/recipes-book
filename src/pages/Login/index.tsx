bbcccd6a41cba5240713cf4f2400d356e2cbbaf4
Daniel Farias <danielfariias15@gmail.com>
2022-07-27 22:14:20 -0300

    Login converted to typescript

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,8 @@
-import React, { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { SaveUserAtLocalStorage } from '../services/localstorage';
+import LocalStorageService from '../../services/LocalStorageService';
+import { isEmailValid } from '../../utils/isValidEmail';
 
-function isEmailValid(email) {
-  // eslint-disable-next-line max-len
-  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regex.test(email);
-}
 const MIN_INPUT_PASSWORD = 6;
 
 export default function Login() {
@@ -16,17 +12,19 @@ export default function Login() {
   const history = useHistory();
 
   function isValidForm() {
-    if (!inputEmail) return false;
-
-    if (isEmailValid(inputEmail) && InputPassword.length > MIN_INPUT_PASSWORD) {
-      return true;
+    if (inputEmail) {
+      if (isEmailValid(inputEmail) && InputPassword.length > MIN_INPUT_PASSWORD) {
+        return true;
+      }
     }
+
+    return false;
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-    SaveUserAtLocalStorage(inputEmail);
+    LocalStorageService.set('user', { email: inputEmail });
 
     history.push('/foods');
   }
@@ -37,7 +35,6 @@ export default function Login() {
         <input
           id="email-input"
           type="email"
-          data-testid="email-input"
           value={inputEmail}
           onChange={(e) => setInputEmail(e.target.value)}
         />
@@ -47,14 +44,12 @@ export default function Login() {
         <input
           id="password-input"
           type="password"
-          data-testid="password-input"
           value={InputPassword}
           onChange={(e) => setInputPassword(e.target.value)}
         />
       </label>
       <button
         type="submit"
-        data-testid="login-submit-btn"
         disabled={!isValidForm()}
       >
         Enter
--- src/services/localstorage.js
+++
@@ -1,7 +0,0 @@
-export function SaveUserAtLocalStorage(email) {
-  localStorage.setItem('user', JSON.stringify({ email }));
-  localStorage.setItem('mealsToken', JSON.stringify(1));
-  localStorage.setItem('cocktailsToken', JSON.stringify(1));
-}
-
-export const aaaaaa = '';
