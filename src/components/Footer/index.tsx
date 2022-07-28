97573a35c58b873fe0e6097feb3f0dd2969c4756
Daniel Farias <danielfariias15@gmail.com>
2022-07-27 22:08:57 -0300

    Converted Footer to typescript

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,21 +1,19 @@
 import { Link } from 'react-router-dom';
 
-import drinkIcon from '../images/drinkIcon.svg';
-import mealIcon from '../images/mealIcon.svg';
-
-const styles = {
-  position: 'fixed',
-  width: '100%',
-  maxWidth: 428,
-  backgroundColor: '#ff0',
-  bottom: 0,
-  left: 0,
-};
+import drinkIcon from '../../images/drinkIcon.svg';
+import mealIcon from '../../images/mealIcon.svg';
 
 export default function Footer() {
   return (
     <footer
-      style={styles}
+      style={{
+        position: 'fixed',
+        width: '100%',
+        maxWidth: 428,
+        backgroundColor: '#ff0',
+        bottom: 0,
+        left: 0,
+      }}
     >
       <Link to="/drinks">
         <img src={drinkIcon} alt="" />
